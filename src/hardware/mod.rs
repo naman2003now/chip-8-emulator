4fe5f8ea9a094b03f95648953ed0cf5db896c72d
Naman Agrawal <naman2003now@gmail.com>
2024-06-07 20:00:51 +0530

    create struct for hardware specification
    - memory 4kb
    - display 64px x 32px
    - pc (Program Counter) 16bit
    - i (Index Register) 16bit
    - delay_timer 8bit
    - sounder_timer 8bit
    - 16 x registers (General Purpose) 8bit

---
+++ src/hardware/mod.rs
@@ -0,0 +1,82 @@
+pub struct Hardware {
+    memory: [u8; 4096],
+    display: [u64; 32],
+    pc: u16,
+    i: u16,
+    delay_timer: u8,
+    sound_timer: u8,
+    registors: [u8; 16],
+}
+
+impl Hardware {
+    pub fn new() -> Self {
+        let memory = [0; 4096];
+        let display = [0; 32];
+        let pc = 0;
+        let i = 0;
+        let delay_timer = 0;
+        let sound_timer = 0;
+        let registors = [0; 16];
+
+        Self {
+            memory,
+            display,
+            pc,
+            i,
+            delay_timer,
+            sound_timer,
+            registors,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Hardware;
+    use std::any::{type_name, type_name_of_val};
+    use std::mem::size_of_val;
+
+    #[test]
+    fn memory_size() {
+        let hardware = Hardware::new();
+        assert_eq!(size_of_val(&hardware.memory), 4096);
+    }
+
+    #[test]
+    fn display() {
+        let hardware = Hardware::new();
+        assert_eq!(hardware.display.len(), 32);
+        assert_eq!(type_name_of_val(&hardware.display[0]), type_name::<u64>());
+    }
+
+    #[test]
+    fn registers() {
+        let hardware = Hardware::new();
+        assert_eq!(hardware.registors.len(), 16);
+        assert_eq!(type_name_of_val(&hardware.registors[0]), type_name::<u8>());
+    }
+
+    #[test]
+    fn pc() {
+        let hardware = Hardware::new();
+        assert_eq!(type_name_of_val(&hardware.pc), type_name::<u16>());
+    }
+
+    #[test]
+    fn i() {
+        let hardware = Hardware::new();
+        assert_eq!(type_name_of_val(&hardware.i), type_name::<u16>());
+    }
+
+    #[test]
+    fn delay_timer() {
+        let hardware = Hardware::new();
+        assert_eq!(type_name_of_val(&hardware.delay_timer), type_name::<u8>());
+    }
+
+    #[test]
+    fn sound_timer() {
+        let hardware = Hardware::new();
+        assert_eq!(type_name_of_val(&hardware.sound_timer), type_name::<u8>());
+    }
+}
